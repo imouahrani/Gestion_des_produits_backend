8c7dfc4cf063726096094d33abec26e9735bf103
Imene <imene.ouahrani@capgemini.com>
2023-09-22 17:37:33 +0200

    Atelier 04 : Spring boot: le Design pattern DTO (Data Transfer Object)

--- src/main/java/com/iouahran/produits/service/ProduitService.java
+++ src/main/java/com/iouahran/produits/service/ProduitService.java
@@ -2,16 +2,17 @@ package com.iouahran.produits.service;
 
 import com.iouahran.produits.entities.Categorie;
 import com.iouahran.produits.entities.Produit;
+import com.iouahran.produits.entities.ProduitDTO;
 
 import java.util.List;
 
 public interface ProduitService {
-    Produit saveProduit(Produit p);
+   // Produit saveProduit(Produit p);
     Produit updateProduit(Produit p);
     void deleteProduit(Produit p);
     void deleteProduitById(Long id);
-    Produit getProduit(Long id);
-    List<Produit> getAllProduits();
+   // Produit getProduit(Long id);
+   // List<Produit> getAllProduits();
     List<Produit> findByNomProduit(String nom);
     List<Produit> findByNomProduitContains(String nom);
     List<Produit> findByNomPrix (String nom, Double prix);
@@ -19,4 +20,10 @@ public interface ProduitService {
     List<Produit> findByCategorieIdCat(Long id);
     List<Produit> findByOrderByNomProduitAsc();
     List<Produit> trierProduitsNomsPrix();
+
+    ProduitDTO convertEntityToDto (Produit produit);
+    ProduitDTO saveProduit(Produit p);
+    ProduitDTO getProduit(Long id);
+    List<ProduitDTO> getAllProduits();
+    Produit convertDtoToEntity(ProduitDTO produitDto);
 }
